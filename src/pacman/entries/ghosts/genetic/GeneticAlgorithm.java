70dd4e5278fb79baeb054e0d7975e2634d06965d
Julian Toledano <jtoledanodiaz@gmail.com>
2017-06-21 19:20:39 +0200

    Genotipo

--- src/pacman/entries/ghosts/genetic/GeneticAlgorithm.java
+++ src/pacman/entries/ghosts/genetic/GeneticAlgorithm.java
@@ -3,6 +3,7 @@ package pacman.entries.ghosts.genetic;
 import static pacman.game.Constants.DELAY;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.EnumMap;
 import java.util.Random;
 
@@ -15,35 +16,44 @@ import pacman.game.Constants.MOVE;
 
 // Crear la primera generación 
 public class GeneticAlgorithm {
-	ArrayList <Individuo> poblacion;
+	private ArrayList <Individuo> poblacion;
 	private int numeroDeIndividuos;
+	private double []genotipo;
 	
 	public GeneticAlgorithm(Game game, int numeroDeIndividuos){
 		this.poblacion = new ArrayList<Individuo>();
 		this.numeroDeIndividuos = numeroDeIndividuos;
+		genotipo = new double[4];
 		crearPoblacion(numeroDeIndividuos, game);
 	}
 	
-	public void inicializarPoblacion(){
-		for(int i = 0; i < numeroDeIndividuos; i++){
-			poblacion.get(i).setAfinidad(entrenar(new StarterPacMan(), new MyGhosts(poblacion.get(i).getQ()), 100));
-			System.out.println(poblacion.get(i).toString() + poblacion.get(i).getAfinidad());
-		}
-			
-	}
+	
 	private void crearPoblacion(int numeroDeIndividuos, Game game){
 		for(int i = 0; i < numeroDeIndividuos; i++){
-			double value = generarValorQ(20);
-			double eps = Math.random();
-			double alpha = Math.random();
-			double gamma = Math.random();
-			poblacion.add(new Individuo(game, value, eps, alpha, gamma));
-			
+			genotipo[0] = generarValorQ(20);
+			genotipo[1] = Math.random();
+			genotipo[2] = Math.random();
+			genotipo[3] = Math.random();
+			poblacion.add(new Individuo(game, genotipo[0], genotipo[1], genotipo[2], genotipo[3]));		
 		}
-		
+		inicializarPoblacion();
 		//for(int i = 0; i < poblacion.size(); i++)
 			//System.out.println(poblacion.get(i).getAfinidad());
 	}
+	
+	private void inicializarPoblacion(){
+		for(int i = 0; i < numeroDeIndividuos; i++)
+			poblacion.get(i).setAfinidad(entrenar(new StarterPacMan(), new MyGhosts(poblacion.get(i).getQ()), 1000));
+		Collections.sort(poblacion);
+		for(int i = 0; i < numeroDeIndividuos; i++)
+			System.out.println(poblacion.get(i).toString() + poblacion.get(i).getAfinidad());
+	}
+	
+	public void generarDescendencia(){
+		ArrayList<Individuo>descendientes = new ArrayList<Individuo>();
+		
+	}
+	
 	
 	private int generarValorQ(int max){
 		int random = (int) (Math.random() * max);
@@ -73,7 +83,7 @@ public class GeneticAlgorithm {
 			}
 			
 			//System.out.println(avgScore/trials);
-			return avgScore;
+			return avgScore/trials;
 
 	    }
 
--- src/pacman/entries/ghosts/genetic/Individuo.java
+++ src/pacman/entries/ghosts/genetic/Individuo.java
@@ -3,13 +3,13 @@ package pacman.entries.ghosts.genetic;
 import pacman.entries.ghosts.Qlearning;
 import pacman.game.Game;
 
-public class Individuo {
+public class Individuo implements Comparable<Individuo>{
 	private Qlearning q;
 	private double value;
 	private double eps;
 	private double alpha;
 	private double gamma;
-	private double afinidad;
+	private Double afinidad;
 	
 	public Individuo(Game game, double value, double eps, double alpha, double gamma){
 		this.value = value;
@@ -17,11 +17,17 @@ public class Individuo {
 		this.alpha = alpha;
 		this.gamma = gamma;
 		this.q = new Qlearning(game, value, eps, alpha, gamma);
-		this.afinidad = 0;
+		this.afinidad = 0.0;
 	}
 	
 	public Qlearning getQ(){return q;}
 	public double getAfinidad(){return afinidad;}
 	public void setAfinidad(double afinidad){this.afinidad = afinidad;}
 	public String toString(){return value + ";" + eps + ";" + alpha + ";" + gamma + ";";}
+
+	@Override
+	public int compareTo(Individuo individuo) {
+		// TODO Auto-generated method stub
+		return this.afinidad.compareTo(individuo.getAfinidad());
+	}
 }
